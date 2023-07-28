5dfe43338697b4a9f2b5c2c6ed68619c8eb73728
TaylorBisset <87632154+TaylorBisset@users.noreply.github.com>
2023-07-28 15:48:47 -0700

    Add assignment instructions

--- CS131-RemoveNonAlphas/CS131-RemoveNonAlphas.cpp
+++ CS131-RemoveNonAlphas/CS131-RemoveNonAlphas.cpp
@@ -1,3 +1,17 @@
+/*
+Write a program that removes all non-alphabetic characters from the given input.
+
+Ex: If the input is:
+
+-Hello, 1 world$!
+the output is:
+
+Helloworld
+Your program must define and call the following function that takes a string as a 
+parameter and returns the string without any non-alphabetic characters.
+string RemoveNonAlpha(string userString)
+*/
+
 #include <iostream>
 
 int main()
